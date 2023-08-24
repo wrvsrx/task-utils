8128ff22e672de756f1904031cd258587eadb5f4
wrvsrx <wrvsrx@outlook.com>
2023-08-24 16:22:19 +0800

    feat(app/Cli): improve cli help message

--- app/Cli.hs
+++ app/Cli.hs
@@ -37,7 +37,7 @@ visParser =
               <> help "Tags to be highlighted."
           )
         <*> flag False True (long "impure" <> short 'i' <> help "Enable impure mode. In impure mode, dependency closure will be visualized.")
-        <*> flag False True (long "outside" <> short 'o' <> help "Show tasks not appearing in input json.")
+        <*> flag False True (long "outside" <> short 'o' <> help "Show tasks not appearing in input json as `outside`.")
         <*> flag False True (long "deleted" <> short 'd' <> help "Show deleted tasks.")
    in
     Right <$> vis
