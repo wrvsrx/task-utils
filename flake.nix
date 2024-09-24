9fc177854014b5e96fce72a156b3c54b5d98890e
wrvsrx <wrvsrx@outlook.com>
2024-09-24 18:48:00 +0800

    chore(env): add haskell-language-server

--- flake.nix
+++ flake.nix
@@ -7,19 +7,27 @@
     flake-parts.follows = "flake-lock/flake-parts";
   };
 
-  outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } ({ withSystem, ... }: {
-    systems = [ "x86_64-linux" ];
-    perSystem = { pkgs, ... }: rec {
-      packages.default = pkgs.haskellPackages.callPackage ./default.nix { };
-      devShells.default = pkgs.mkShell {
-        inputsFrom = [ packages.default.env ];
-        nativeBuildInputs = with pkgs; [
-          haskellPackages.cabal-fmt
-          cabal2nix
-          cabal-install
-        ];
-      };
-      formatter = pkgs.nixpkgs-fmt;
-    };
-  });
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } (
+      { withSystem, ... }:
+      {
+        systems = [ "x86_64-linux" ];
+        perSystem =
+          { pkgs, ... }:
+          rec {
+            packages.default = pkgs.haskellPackages.callPackage ./default.nix { };
+            devShells.default = pkgs.mkShell {
+              inputsFrom = [ packages.default.env ];
+              nativeBuildInputs = with pkgs; [
+                haskellPackages.cabal-fmt
+                cabal2nix
+                cabal-install
+                haskell-language-server
+              ];
+            };
+            formatter = pkgs.nixpkgs-fmt;
+          };
+      }
+    );
 }
